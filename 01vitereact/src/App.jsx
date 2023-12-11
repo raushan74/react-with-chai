502ad727adb6b6b5906c4e6652770b9fc05af7f6
raushanraj7156 <raushanraj7156@gmail.com>
2023-12-11 18:56:54 +0000

    basic vite setup

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,19 @@
 
-
+import Chai from "./Chai";
 function App() {
   
 
   return (
+    <>
     <h1>chai aur react with vite</h1>
+    <Chai/>
+    </>
   )
 }
 
 export default App
+
+
+// In project with vite file extension must be jsx,
+// html function shoule be with first letter capital
+// enclose <>more than one tag</>  --> to render more than one html tag
