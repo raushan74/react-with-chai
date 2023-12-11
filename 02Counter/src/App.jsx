1549a34575f55907ea202ef1ea61a9c29df28a33
raushanraj7156 <raushanraj7156@gmail.com>
2023-12-11 21:00:20 +0000

    counter

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+
+function App() {
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 15
+
+  const upcheckValue = (value) => {
+    if (value >= 6) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  const dncheckValue = (value) => {
+    if (value <= 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  const addValue = () => {
+    //counter = counter + 1
+    if (upcheckValue(counter)) {
+      setCounter(counter + 1)
+    }
+    //setCounter(counter+1))
+
+    //console.log(counter)
+  }
+  const removeValue = () => {
+    if (dncheckValue(counter)) {
+      setCounter(counter - 1)
+    }
+  }
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>Counter value: {counter} </h2>
+      <h3>counter value1: {counter}</h3>
+      <button
+        onClick={addValue}
+      >Add value</button>
+      <br />
+      <button
+        onClick={removeValue}
+      >remove value</button>
+
+    </>
+  )
+}
+
+export default App
